06b0dea2130002cac9353e5a7b9b57487685dbe6
smlnkls1208 <al89994950059@gmail.com>
2025-10-28 15:44:29 +0700

    детали автора и книг

---
+++ catalog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+
+def index(request):
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+    )
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
